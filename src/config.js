417a52cd3ed60dd2efeccafec250a6a2920ef22a
tiktokshorts <136311235+tiktokshorts@users.noreply.github.com>
2024-03-10 20:30:44 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ export default {
    * This is our caching backend
    */
   // This used to work, but seems like GitHub no longer allows large website hosting:
-  areaServer: 'https://anvaka.github.io/index-large-cities/data',
+  areaServer: 'https://tiktokshorts.github.io/index-large-cities/data',
   //areaServer: 'http://localhost:8085', // This is un-commented when I develop cache locally
   // So, using S3
   //areaServer: 'https://city-roads.s3-us-west-2.amazonaws.com/nov-02-2020',
